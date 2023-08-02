bdfb6477695194ba7fabc1879e87204cb222ccd0
Mildred <mtovar001@hotmail.com>
2023-08-01 21:04:14 -0400

    adding new files

--- Assignment/index.js
+++ Assignment/index.js
@@ -1,8 +1,4 @@
 const excelToJson = require ('convert-excel-to-json');
 const fs = require('fs');
 
-const result = excelToJson({
-    source: fs.readFileSync('./data.xlsx') // fs.readFileSync return a Buffer
-});
 
-console.log(result);
